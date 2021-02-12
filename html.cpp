8342a77dab574246568b13086c3dc90c15a6a98c
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:36:18 +0400

    Create html.cpp

---
+++ html.cpp
@@ -0,0 +1,18 @@
+<html>
+/* TODO: FIX HTML */
+
+#define using namespace OZARON;
+using using;
+ozaron; using;
+
+int(main(){
+  #include(ozaror)->ESP(  );
+  ESP( OZAROR./*ozaron#5101 discord subscribe*/OZAROR->ESP( FAKEDUK )  );
+  bSendPacket( *OZAROR->FAKE_ESP( WALHAK ) );
+
+  egiste( "BAYONET" ).r<Bayonet>print( "Bayonet Hacked\a\\a\a\a\a\a\a\a\a\\a\a\a\n" );
+
+    FindSegnature( GetProccessHandle( "BAYONET" )) )->Hacked()->Signature("Ozaron#5101")->AIMBOT()
+});
+
+DWORD INJECT->BSendPacket(ESP);
